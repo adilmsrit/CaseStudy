d6f77908bccc13de7c3e90aa36cf4fa3ea60060e
adilmsrit <adilmsrit@gmail.com>
2018-09-11 16:24:26 +0400

    Update StepDefnition.java

--- src/main/java/StepDefinitions/StepDefnition.java
+++ src/main/java/StepDefinitions/StepDefnition.java
@@ -5,10 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
-
-import static StepDefinitions.CommonFunctions.waitForElement;
-
 public class StepDefnition extends CommonFunctions {
 
 
@@ -30,41 +26,23 @@ public class StepDefnition extends CommonFunctions {
     @When("^he looks at a return trip from DXB to LHR leaving one week from now$")
     public void he_looks_at_a_return_trip_from_DXB_to_LHR_leaving_one_week_from_now() throws InterruptedException {
 
-        String searchingText = "London Heathrow Airport (LHR), London, United Kingdom";
         String partialText = "LHR";
 
-        Thread.sleep(5000);
         WebElement text = waitForElement(By.xpath("//div[@class='widget__panel js-widget-panel panel-active']//input[@name='Arrival airport']"), 20);
         text.sendKeys("LHR");
 
         Thread.sleep(5000);
-        WebElement element =driver.findElement(By.xpath("//div[@class='widget__panel js-widget-panel panel-active']//div[@data-data-set='destinations']//ol[@class='location__list']"));
 
-        List<WebElement> results = element.findElements(By.tagName("li"));
+        WebElement firstElement = driver.findElement(By.xpath("//div[@class='widget__panel js-widget-panel panel-active']//p[@class='location__airport__acronym to-highlight']"));
+        firstElement.click();
 
-        for (WebElement result : results) {
-            if (result.getText().equals(searchingText)) {
-                result.click();
-                System.out.println("Selected: " + result.getText());
-                break;
-            }
-        }
-
-        /*Or use the below to Just click
-        WebElement firstElement = driver.findElement(By.xpath("//div[@class='widget__panel js-widget-panel panel-active']//div[@data-data-set='destinations']//ol[@class='location__list']//li[@class='location__item js-dropdown-select-item highlight js-location-list']"));
-        firstElement.click();*/
-
-
-        //Click on Calendar
-        waitForElement(By.xpath(".//*[@id='search-flight-date-picker--depart']"), 20).click();
-
-        //Select Date using below field.
-        waitForElement(By.xpath("(//eol-calendar[@title='Please choose your departure date']//table[@class='ek-datepicker__table']//td[@class='ek-datepicker__day ek-datepicker__day--start ek-datepicker__today']"), 20).click();
+       //Select Departing Date using below field.
+        waitForElement(By.xpath("//div[@class='widget__panel js-widget-panel panel-active']//eol-calendar[@title='Please choose your departure date']//td[@class='ek-datepicker__day ek-datepicker__today ek-datepicker__day--notselected']"), 20).click();
         //Select return date using below
-        waitForElement(By.xpath(".//*[@id='panel0']/div/div/div/div[2]/section/div[4]/div[1]/div[3]/eol-datefield/eol-calendar/div/div/div[2]/table/tbody/tr[3]/td[7]/a"), 20).click();
+        waitForElement(By.xpath("//div[@class='widget__panel js-widget-panel panel-active']//eol-calendar[@title='Please choose your departure date']//tbody//td[@data-string='1882018']"), 20).click();
         //Click on submit using below.
-        waitForElement(By.xpath(".//*[@id='panel0']/div/div/div/div[2]/section/div[4]/div[2]/div[3]/form/button"), 20).click();
-        
+        waitForElement(By.xpath("//div[@class='widget__panel js-widget-panel panel-active']//button[@type='submit']//span[text()='Search flights']"), 20).click();
+
         System.out.println("Arrival Airport set to LHR");
 
     }
@@ -73,9 +51,7 @@ public class StepDefnition extends CommonFunctions {
     public void he_should_be_shown_the_cheapest_return_ticket_from_DXB_to_LHR() {
         // Write code here goes to next page and fetches the price for the tickets shown.
 
-        waitForElement(By.xpath(".//*[@id='panel0']/div/div/div/div[2]/section/div[4]/div[2]/div[3]/form/button"), 20).click();
-
-        String price = waitForElement(By.xpath("//span[@class='carrier-imposed-curr carrier-imposed-span']"), 20).getText();
+        String price = waitForElement(By.xpath("//div[@id='LowestPriceContainer']//span[@class='summary-curr-only']//span[@class='carrier-imposed-curr carrier-imposed-span']"), 20).getText();
 
         System.out.println("Price : " + price);
         
